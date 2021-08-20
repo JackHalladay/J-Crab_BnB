379eda8ef4fe6734487afd036c5fbe6f645f1ba9
brock789 <brockway789@googlemail.com>
2021-08-20 12:09:22 +0100

    created databases

---
+++ db/04_creating_requests.sql
@@ -0,0 +1,10 @@
+CREATE TABLE requests (
+	id SERIAL PRIMARY KEY,
+	dates_id int,
+	space_id int,
+	date DATE,
+	user_id int,
+	FOREIGN KEY (dates_id) REFERENCES dates (id),
+	FOREIGN KEY (space_id) REFERENCES spaces (id),
+	FOREIGN KEY (user_id) REFERENCES users (id)
+);
